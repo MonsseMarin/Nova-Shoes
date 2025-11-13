1b976248e90c1f0374eae580e6af2798c5de4690
Monsse Marin <monsse.mv19@gmail.com>
2025-11-13 12:13:56 -0600

    se agrego catalogo mujer

--- ProyectoZapatos/catalogo/urls.py
+++ ProyectoZapatos/catalogo/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.catalogo, name='catalogo'),
+    path('catalogomujer/', views.catalogo_mujer, name='catalogo_mujer'),
+    path('catalogohombre/', views.catalogo_hombre, name='catalogo_hombre'),
+    path('catalogoninos/', views.catalogo_ninos, name='catalogo_ninos'),
 ]
--- ProyectoZapatos/catalogo/views.py
+++ ProyectoZapatos/catalogo/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def catalogo(request):
     return render(request, 'catalogo/catalogo.html')
+
+def catalogo_mujer(request):
+    return render(request, 'catalogo/catalogomujer.html')
+
+def catalogo_hombre(request):
+    return render(request, 'catalogo/catalogohombre.html')
+
+def catalogo_ninos(request):
+    return render(request, 'catalogo/catalogoninos.html')
